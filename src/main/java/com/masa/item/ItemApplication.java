3810770206efc4b8800518890a1ff338605072d4
jorge1b3 <jorge1b3@hotmail.es>
2024-10-27 03:02:05 -0500

    feat: add eureka client conection

--- src/main/java/com/masa/item/ItemApplication.java
+++ src/main/java/com/masa/item/ItemApplication.java
@@ -2,12 +2,14 @@ package com.masa.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ItemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ItemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
 }
