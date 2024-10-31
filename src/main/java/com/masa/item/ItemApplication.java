5c64ffda3fa251aaab0bdc7d53e977c558779e3a
jorge1b3 <jorge1b3@hotmail.es>
2024-10-31 15:30:33 -0500

    fix: enable crossOrigin from anywhere using another method

--- src/main/java/com/masa/item/ItemApplication.java
+++ src/main/java/com/masa/item/ItemApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@CrossOrigin(origins = "*")
 public class ItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemApplication.class, args);
